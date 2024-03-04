310c13d82b25d8d74e0754abfd52c4e074bf8186
LichtBlade <risi.bustillo.up@phinmaed.com>
2024-03-04 22:45:56 +0800

    shesh

--- app/src/main/java/com/example/talipafast/MainActivity.kt
+++ app/src/main/java/com/example/talipafast/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.talipafast
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
